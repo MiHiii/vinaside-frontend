c0bdb4283d2581eea3e07b9649aa3b57b97cfb49
ledoan05 <lecongdoan05@gmail.com>
2025-07-18 22:42:33 +0700

    fix ui

---
+++ src/components/payment/PaymentMethodForm.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PaymentMethodForm: React.FC = () => {
+  return (
+    <div className="p-4 border rounded-xl bg-white shadow">
+      <h3 className="text-lg font-semibold mb-2">Phương thức thanh toán</h3>
+      <p className="text-gray-500">(Chức năng chọn phương thức thanh toán sẽ được bổ sung ở đây.)</p>
+    </div>
+  );
+};
+
+export default PaymentMethodForm; 
