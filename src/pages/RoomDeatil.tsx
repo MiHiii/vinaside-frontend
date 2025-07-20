4a693a1e5c91b7cf09ea8b96dac1fdb75eb956ea
QuangPM296 <quangminhpham296@agamil.com>
2025-07-20 11:38:37 +0700

    fix-component-RomDetail

--- src/pages/RoomDeatil.tsx
+++ src/pages/RoomDeatil.tsx
@@ -16,7 +16,6 @@ import RoomInfo from '@/components/roomdetail/RoomInfo';
 import BookingForm from '@/components/roomdetail/BookingForm';
 import { useBookedDates } from '@/hooks/useBookedDates';
 import RoomReviews from '@/components/roomdetail/RoomReviews';
-import RoomDescription from '@/components/roomdetail/RoomDescription';
 
 export default function RoomDetailPage() {
   const { id } = useParams();
@@ -93,14 +92,6 @@ export default function RoomDetailPage() {
           {/* Thông tin phòng và mô tả */}
           <div className='lg:col-span-8 space-y-6 lg:space-y-8'>
             <RoomInfo
-              listing={listingData}
-              amenitiesList={amenitiesList}
-              services={services}
-              selectedServiceIds={selectedServiceIds}
-              setSelectedServiceIds={setSelectedServiceIds}
-              safetyFeatures={safetyFeatures}
-            />
-            <RoomDescription
               listing={listingData}
               amenitiesList={amenitiesList}
               services={services}
