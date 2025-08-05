fba00f3b974c67cc5ae74694cd067e91b2264382
QuangPM296 <quangminhpham296@agamil.com>
2025-08-05 16:55:49 +0700

    listing-status

--- src/store/slices/dashboardSlice.ts
+++ src/store/slices/dashboardSlice.ts
@@ -25,7 +25,6 @@ export interface DashboardData {
     listingsByStatus: {
       active: number;
       inactive: number;
-      draft: number;
     };
     totalBookings: number;
     totalRevenue: number;
